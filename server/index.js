c2518bd1f156593aab2ca75d2fbc7a37394e31d1
Chris Nguyen <chrishiepnguyen@yahoo.com>
2023-12-16 11:47:12 -0600

    connected mongodb to app

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const port = 1337;
 
+app.use(cors());
+app.use(express.json());
+
+dotenv.config()
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log('DB connected successfully'))
+    .catch(err => {
+        console.log(err)
+    });
 
 
 app.get('/', (req, res) => {
@@ -14,4 +28,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log('Server is running on port', port)
-})
+});

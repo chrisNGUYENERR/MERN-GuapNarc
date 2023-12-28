9d5ef0b92b611186924dfe3f563da463bf924d28
Chris Nguyen <chrishiepnguyen@yahoo.com>
2023-12-28 15:42:15 -0600

    finished account register

--- server/models/User.js
+++ server/models/User.js
@@ -4,9 +4,10 @@ const UserSchema = new mongoose.Schema(
     {
         firstName: { type: String, required: true },
         lastName: { type: String, required: true },
-        email: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
-        checkTerms: { type: Boolean, required: true }
+        checkTerms: { type: Boolean, required: true },
+        itemList: Array
     },
     { collection: 'users'}
 );
